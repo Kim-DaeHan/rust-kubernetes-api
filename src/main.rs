2564e3f4bd080a247b6e005fd1a50b328d2e4396
Kim-DaeHan <Kim-DaeHan@users.noreply.github.com>
2024-01-24 18:46:39 +0900

    .

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,18 @@
 use clap::Parser;
+use k8s_openapi::api::apps::v1::Deployment;
 use k8s_openapi::api::core::v1::Pod;
 use kube::{
-    api::{Api, ListParams, PostParams, ResourceExt},
+    api::{Api, ListParams, Patch, PatchParams, ResourceExt},
     Client,
 };
 use serde::Deserialize;
-use serde_json::Value;
+use serde_json::json;
 
 #[derive(Debug, Deserialize)]
 struct JsonPatch {
     op: String,
     path: String,
     value: u32,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct DeploymentSpec {
-    replicas: u32,
-}
-
-#[derive(Debug, serde::Serialize, serde::Deserialize)]
-struct Deployment {
-    spec: DeploymentSpec,
 }
 
 #[derive(Parser, Debug)]
@@ -37,18 +28,51 @@ struct Cli {
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
 
-    let path: JsonPatch = serde_json::from_str(&cli.path).expect("failed");
+    let cli_path: JsonPatch = serde_json::from_str(&cli.path).expect("failed");
+    let op = cli_path.op;
+    let value = cli_path.value;
+    let path = cli_path.path;
+
     let deployment_name = cli.name;
 
     let client = Client::try_default().await?;
 
+    // Deployment에 접근하기 위해 Api 객체 생성
+    let deployments: Api<Deployment> = Api::default_namespaced(client.clone());
+    // 생성한 Deployment들 확인 하는 코드
+    for d in deployments.list(&ListParams::default()).await? {
+        println!("found deployment {}", d.name_any());
+    }
+
+    // Deployment 이름으로부터 Deployment 리소스 가져오기
+    let deployment = deployments.get(&deployment_name).await?;
+
+    let patch_params = PatchParams {
+        field_manager: Some("my-deployment".to_string()),
+        ..Default::default()
+    };
+    let patch_data = json!({
+        "op": op,
+        "path": path,
+        "value": value,
+    });
+    let patch_data = Patch::Apply(&patch_data);
+
+    // Deployment 수정
+    deployments
+        .patch(&deployment_name, &patch_params, &patch_data)
+        .await?;
+
+    // 수정된 Deployment 확인 또는 다른 작업 수행
+    let modified_deployment = deployments.get(&deployment_name).await?;
+    println!("Modified Deployment: {:?}", modified_deployment);
+
+    // 생성한 pod 확인 하는 코드
     let pods: Api<Pod> = Api::default_namespaced(client);
+
     for p in pods.list(&ListParams::default()).await? {
         println!("found pod {}", p.name_any());
     }
-
-    println!("{:?}", path);
-    println!("{:?}", deployment_name);
 
     Ok(())
 }
